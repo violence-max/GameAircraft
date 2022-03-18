2ba6d8567f140d000b895d887b91545c62a6a0a7
violece.max <2016502436@qq.com>
2022-03-19 00:43:26 +0800

    feat:击杀敌机后有一定概率产生三种不同的道具

---
+++ src/edu/hitsz/Prop/HpProp.java
@@ -0,0 +1,15 @@
+package edu.hitsz.Prop;
+
+public class HpProp extends AbstractProp{
+
+    private int hp;
+
+    public HpProp(int locationX,int locationY,int speedX,int speedY){
+        super(locationX,locationY,speedX,speedY);
+    }
+
+    public void increasehp(){
+        ;
+    }
+
+}
